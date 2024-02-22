06cbc037b0e1db2e3202d840fe02aed414b01937
cheezi0747 <linus.sjolinder@b3.se>
2024-02-22 11:14:22 +0100

    UF-7191: Handle if externalMessages are null

--- src/main/java/se/sundsvall/webmessagecollector/integration/opene/OpenEMapper.java
+++ src/main/java/se/sundsvall/webmessagecollector/integration/opene/OpenEMapper.java
@@ -1,11 +1,14 @@
 package se.sundsvall.webmessagecollector.integration.opene;
 
+import static java.util.Collections.emptyList;
+
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import java.util.List;
+import java.util.Optional;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
@@ -20,51 +23,55 @@ import se.sundsvall.webmessagecollector.integration.opene.model.Messages;
 
 @Component
 class OpenEMapper {
-    private static final Logger LOG = LoggerFactory.getLogger(OpenEMapper.class);
-    
-    private static final DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-        .appendPattern("yyyy-MM-dd")
-        .optionalStart()
-        .appendPattern(" HH:mm")
-        .optionalEnd()
-        .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
-        .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
-        .toFormatter();
-    
-    List<MessageEntity> mapMessages(byte[] errands, String familyId) {
-        
-        if (errands == null) {
-            return List.of();
-        }
-        
-        var xmlString = new String(errands, StandardCharsets.ISO_8859_1);
-        try {
-            return new XmlMapper()
-                .readValue(xmlString, Messages.class)
-                .getExternalMessages().stream()
-                .filter(externalMessage -> !externalMessage.isPostedByManager())
-                .map(externalMessage -> {
-                    var entity = MessageEntity.builder()
-                        .withFamilyId(familyId)
-                        .withDirection(externalMessage.isPostedByManager() ? Direction.OUTBOUND : Direction.INBOUND)
-                        .withMessageId(String.valueOf(externalMessage.getMessageID()))
-                        .withExternalCaseId(String.valueOf(externalMessage.getFlowInstanceID()))
-                        .withMessage(externalMessage.getMessage())
-                        .withSent(LocalDateTime.parse(externalMessage.getAdded(), formatter));
-                    if (externalMessage.getPoster() != null) {
-                        entity.withEmail(externalMessage.getPoster().getEmail())
-                            .withFirstName(externalMessage.getPoster().getFirstname())
-                            .withLastName(externalMessage.getPoster().getLastname())
-                            .withUsername(externalMessage.getPoster().getUsername())
-                            .withUserId(String.valueOf(externalMessage.getPoster().getUserID()));
-                    }
-                    return entity.build();
-                })
-                
-                .toList();
-        } catch (JsonProcessingException e) {
-            LOG.info("Something went wrong parsing messages", e);
-            return List.of();
-        }
-    }
+
+	private static final Logger LOG = LoggerFactory.getLogger(OpenEMapper.class);
+
+	private static final DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+		.appendPattern("yyyy-MM-dd")
+		.optionalStart()
+		.appendPattern(" HH:mm")
+		.optionalEnd()
+		.parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+		.parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+		.toFormatter();
+
+	List<MessageEntity> mapMessages(final byte[] errands, final String familyId) {
+
+		if (errands == null) {
+			return List.of();
+		}
+
+		final var xmlString = new String(errands, StandardCharsets.ISO_8859_1);
+		try {
+			return
+				Optional.ofNullable(new XmlMapper()
+						.readValue(xmlString, Messages.class)
+						.getExternalMessages())
+					.orElse(emptyList()).stream()
+					.filter(externalMessage -> !externalMessage.isPostedByManager())
+					.map(externalMessage -> {
+						final var entity = MessageEntity.builder()
+							.withFamilyId(familyId)
+							.withDirection(externalMessage.isPostedByManager() ? Direction.OUTBOUND : Direction.INBOUND)
+							.withMessageId(String.valueOf(externalMessage.getMessageID()))
+							.withExternalCaseId(String.valueOf(externalMessage.getFlowInstanceID()))
+							.withMessage(externalMessage.getMessage())
+							.withSent(LocalDateTime.parse(externalMessage.getAdded(), formatter));
+						if (externalMessage.getPoster() != null) {
+							entity.withEmail(externalMessage.getPoster().getEmail())
+								.withFirstName(externalMessage.getPoster().getFirstname())
+								.withLastName(externalMessage.getPoster().getLastname())
+								.withUsername(externalMessage.getPoster().getUsername())
+								.withUserId(String.valueOf(externalMessage.getPoster().getUserID()));
+						}
+						return entity.build();
+					})
+
+					.toList();
+		} catch (final JsonProcessingException e) {
+			LOG.info("Something went wrong parsing messages", e);
+			return List.of();
+		}
+	}
+
 }
