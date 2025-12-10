e79fc338f1fc4790660b2d6a8adc888581bf0c68
Linus Sjölinder <linus.sjolinder@b3.se>
2025-12-10 13:16:16 +0100

    update to dept44 7.0

--- src/main/java/se/sundsvall/webmessagecollector/api/model/MessageAttachment.java
+++ src/main/java/se/sundsvall/webmessagecollector/api/model/MessageAttachment.java
@@ -8,16 +8,16 @@ import lombok.Getter;
 @Builder(setterPrefix = "with")
 public class MessageAttachment {
 
-	@Schema(description = "The Id for the attachment", example = "1")
+	@Schema(description = "The Id for the attachment", examples = "1")
 	private final Integer attachmentId;
 
-	@Schema(description = "The name of the file", example = "file.txt")
+	@Schema(description = "The name of the file", examples = "file.txt")
 	private final String name;
 
-	@Schema(description = "The extension of the file", example = "txt")
+	@Schema(description = "The extension of the file", examples = "txt")
 	private final String extension;
 
-	@Schema(description = "The mime type of the file", example = "text/plain")
+	@Schema(description = "The mime type of the file", examples = "text/plain")
 	private final String mimeType;
 
 }
